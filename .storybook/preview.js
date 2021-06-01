6e0bb31d089c0a7021a42d1dba01ea639114b9cd
Hiduíno Domingos <hvduino@gmail.com>
2021-06-01 14:25:18 -0300

    chore: boilerplate

---
+++ .storybook/preview.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../src/styles/theme'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#181b23',
+      },
+      {
+        name: 'light',
+        value: '#f2f2f2',
+      },
+    ],
+  },
+}
+
+const withChakra = (StoryFn) => {
+
+  return (
+    <ChakraProvider theme={theme}>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
+      <StoryFn />
+    </ChakraProvider>
+  )
+}
+
+export const decorators = [withChakra]
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,19 @@
+import { Text, Button } from '@chakra-ui/react'
+
+export interface Props {
+  title: string
+  description: string
+}
+export default function Main({ title, description }: Props) {
+  return (
+    <>
+      <Text as="h1">{title}</Text>
+      <Text>{title}</Text>
+      <Text>{description}</Text>
+      <Text color="red.500">{description}</Text>
+      <Button p={4} colorScheme="pink">
+        OKOK
+      </Button>
+    </>
+  )
+}
