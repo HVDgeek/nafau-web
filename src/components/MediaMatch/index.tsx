8f0c7e35fbc70c37d2cefedc379ff63c5036e75f
Hiduíno Domingos <hvduino@gmail.com>
2021-06-03 10:45:19 -0300

    Media Match component

---
+++ src/components/MediaMatch/index.tsx
@@ -0,0 +1,28 @@
+import { Box } from '@chakra-ui/react'
+import styled, { css } from 'styled-components'
+import media, { DefaultBreakpoints } from 'styled-media-query'
+
+type breakpoint = keyof DefaultBreakpoints
+
+export type MediaMatchProps = {
+  lessThan?: breakpoint
+  greaterThan?: breakpoint
+}
+
+const mediaMatchModifiers = {
+  lessThan: (size: breakpoint) => css`
+    ${media.lessThan(size)` display: block `}
+  `,
+
+  greaterThan: (size: breakpoint) => css`
+    ${media.greaterThan(size)` display: block `}
+  `
+}
+
+export default styled(Box)<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+  `}
+`
