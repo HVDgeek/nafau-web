3263f28a3c0385769681c70b032ced58237485a4
Hiduíno Domingos <hvduino@gmail.com>
2021-06-04 15:35:44 -0300

    Change fonts to Poppins

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -23,11 +23,13 @@ const withChakra = (StoryFn) => {
 
   return (
     <ChakraProvider theme={theme}>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
         rel="stylesheet"
-      />
+      /> */}
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet"/>
       <StoryFn />
     </ChakraProvider>
   )
