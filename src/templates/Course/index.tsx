e1cd8d5ef427c906914a315afdac8fc448ad28bd
Hiduíno Domingos <hvduino@gmail.com>
2021-06-28 08:08:06 -0300

    Add Empty on my coures page

--- src/templates/Course/index.tsx
+++ src/templates/Course/index.tsx
@@ -4,12 +4,13 @@ import Heading from 'components/Heading'
 import Base from 'templates/Base'
 import themes from 'styles/alt-themes'
 import ClassCard, { ClassCardProps } from 'components/ClassCard'
+import Empty from 'components/Empty'
 
 export type CourseTemplateProps = {
   courses: ClassCardProps[]
 }
 
-const Course = ({ courses }: CourseTemplateProps) => {
+const Course = ({ courses = [] }: CourseTemplateProps) => {
   return (
     <Base>
       <Container>
@@ -18,17 +19,25 @@ const Course = ({ courses }: CourseTemplateProps) => {
             Minhas turmas
           </Heading>
         </Box>
-        <VStack maxW="700px" margin="auto" mt={8} spacing={6}>
-          {courses?.map((course, index) => (
-            <Box
-              maxWidth={['300px', '700px']}
-              w="full"
-              key={`${index} - ${course.title}`}
-            >
-              <ClassCard {...course} />
-            </Box>
-          ))}
-        </VStack>
+        {courses?.length ? (
+          <VStack maxW="700px" margin="auto" mt={8} spacing={6}>
+            {courses?.map((course, index) => (
+              <Box
+                maxW={['300px', '700px']}
+                w="full"
+                key={`${index} - ${course.title}`}
+              >
+                <ClassCard {...course} />
+              </Box>
+            ))}
+          </VStack>
+        ) : (
+          <Empty
+            title="Você ainda não tem turmas!"
+            description="Você precisa estar inscrito em alguma turma para que apareça aqui. Abraços 😃"
+            hasLink
+          />
+        )}
       </Container>
     </Base>
   )
