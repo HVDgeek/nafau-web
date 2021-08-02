215c0fd1ebbe7f0ca676fb2ed9950b0725b7097a
Hiduíno Domingos <hvduino@gmail.com>
2021-08-01 23:45:28 -0300

    Using apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,29 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from 'utils/apollo'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApolloClient)
+
   return (
-    <ChakraProvider theme={theme}>
-      <Head>
-        <title>NAFAU</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A melhor Plataforma de Ensino e Aprendizagem 🚀"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Head>
+          <title>NAFAU</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="A melhor Plataforma de Ensino e Aprendizagem 🚀"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   )
 }
 
--- src/pages/manager/students.tsx
+++ src/pages/manager/students.tsx
@@ -1,7 +1,23 @@
+import { gql, useQuery } from '@apollo/client'
 import UsersTemplate, { UsersTemplateProps } from 'templates/Users'
 import usersMock from 'components/UserCard/mock'
 
 export default function StudentsPage(props: UsersTemplateProps) {
+  const { data, loading, error } = useQuery(gql`
+    query getAllAlunos {
+      alunos {
+        id
+        name
+      }
+    }
+  `)
+
+  if (loading) return <p>Carregando....</p>
+
+  if (error) return <p>{error}</p>
+
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
+
   return <UsersTemplate {...props} />
 }
 
