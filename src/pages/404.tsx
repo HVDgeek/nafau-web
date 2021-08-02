5b3d38a6d40941fe4849a567264c3123cbb8424f
Hiduíno Domingos <hvduino@gmail.com>
2021-08-01 21:26:31 -0300

    Not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import { Container } from '@chakra-ui/react'
+import Empty from 'components/Empty'
+import Base from 'templates/Base'
+
+export default function Page404() {
+  return (
+    <Base>
+      <Container>
+        <Empty
+          title="404: Não encontrada!"
+          description="Ops.... esta página não existe"
+          hasLink
+        />
+      </Container>
+    </Base>
+  )
+}
