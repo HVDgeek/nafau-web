86dd51719c1d48006861a429e53bc3d2a750f0b5
Hiduíno Domingos <hvduino@gmail.com>
2021-10-09 21:17:14 -0300

    Configura SignIn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import NextNprogress from 'nextjs-progressbar'
+import { Provider as AuthProvider } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
@@ -10,28 +11,30 @@ function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloClient)
 
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <Head>
-          <title>NAFAU</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="A melhor Plataforma de Ensino e Aprendizagem 🚀"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ChakraProvider theme={theme}>
+          <Head>
+            <title>NAFAU</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="A melhor Plataforma de Ensino e Aprendizagem 🚀"
+            />
+          </Head>
+          <NextNprogress
+            color="#F231A5"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={5}
+            showOnShallow={true}
           />
-        </Head>
-        <NextNprogress
-          color="#F231A5"
-          startPosition={0.3}
-          stopDelayMs={200}
-          height={5}
-          showOnShallow={true}
-        />
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 
